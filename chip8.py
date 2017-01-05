4c3220779bf03f9f03f2daa6bf152bb75aa3b23d
Leo Lam <leolam.cy@gmail.com>
2017-01-05 13:27:25 -0500

    implement most opcodes

--- chip8.py
+++ chip8.py
@@ -1,203 +1,309 @@
+import random
+
+
 class Chip8(object):
+    ERROR_UNKNOWN_OPCODE = "ERROR: unknown opcode"
+    running = False
+
+    font_set = [
+        0xF0, 0x90, 0x90, 0x90, 0xF0,
+        0x20, 0x60, 0x20, 0x20, 0x70,
+        0xF0, 0x10, 0xF0, 0x80, 0xF0,
+        0xF0, 0x10, 0xF0, 0x10, 0xF0,
+        0x90, 0x90, 0xF0, 0x10, 0x10,
+        0xF0, 0x80, 0xF0, 0x10, 0xF0,
+        0xF0, 0x80, 0xF0, 0x90, 0xF0,
+        0xF0, 0x10, 0x20, 0x40, 0x40,
+        0xF0, 0x90, 0xF0, 0x90, 0xF0,
+        0xF0, 0x90, 0xF0, 0x10, 0xF0,
+        0xF0, 0x90, 0xF0, 0x90, 0x90,
+        0xE0, 0x90, 0xE0, 0x90, 0xE0,
+        0xF0, 0x80, 0x80, 0x80, 0xF0,
+        0xE0, 0x90, 0x90, 0x90, 0xE0,
+        0xF0, 0x80, 0xF0, 0x80, 0xF0,
+        0xF0, 0x80, 0xF0, 0x80, 0x80
+    ]
 
     def __init__(self):
         self.reset()
 
     def reset(self):
         self.pc = 0x200
-        self.v = 16 * [0x00]
-        self.i = 0x0000
-        self.memory = 512 * [0x00]
+        self.v = 16 * [0]
+        self.i = 0
+        self.memory = 512 * [0]
         self.delay_timer = None
         self.sound_timer = None
-        self.stack = 16 * [0x0000]
+        self.stack = 16 * [0]
         self.sp = 0
-        self.keys = 16 * [0x00]
+        self.keys = 16 * [0]
+
+        for i, font in enumerate(self.font_set):
+            self.memory[i] = hex(font)
 
     def load_rom(self, rom):
         for i in range(0, len(rom), 2):
             self.memory.append(rom[i:i + 2])
 
     def emulateCycle(self):
-        print(
-            "reading:" + hex(int(self.memory[self.pc] + self.memory[self.pc + 1], 16)))
+        # print(self.memory)
+        # print(
+        #     "reading:" + hex(int(self.memory[self.pc] + self.memory[self.pc + 1], 16)))
+        self.running = True
         self.read_execute_instruction(
             self.memory[self.pc] + self.memory[self.pc + 1])
 
-    def read_execute_instruction(self, instruction):
-        def bit_and(self, instruction, and_target):
-            return format((int(instruction, 16) & and_target), 'x')
+        if self.delay_timer > 0:
+            self.delay_timer -= 1
 
-        inst = hex(int(instruction, 16) & 0xf000)
-        print(inst)
-        if inst == "0x0":
-            if inst == "0x00E0":
+        if self.sound_timer > 0:
+            # make sound
+            self.sound_timer -= 1
+
+    def read_execute_instruction(self, instruction):
+        def bit_and(instruction, and_target):
+            # return format((int(instruction, 16) & and_target), 'x')
+            # return int
+            return int(instruction, 16) & and_target
+
+        inst = format(int(instruction, 16) & 0xf000, "#06x")
+        x = bit_and(instruction, 0x0f00) >> 8
+        y = bit_and(instruction, 0x00f0) >> 4
+        n = bit_and(instruction, 0x000f)
+        nn = bit_and(instruction, 0x00ff)
+        nnn = bit_and(instruction, 0x0fff)
+
+        if inst == "0x000":
+            if y == int("0x00e0", 16):
                 # clear disp
                 pass
-            elif inst == "0x00EE":
+            elif nn == int("0x00ee", 16):
                 # return from subroutine
-                pass
-        elif inst == "0x1":
+                self.sp -= 1
+                self.pc = self.stack[self.sp]
+            else:
+                self.running = False
+
+        elif inst == "0x1000":
             # goto addr NNN
-            nnn = bit_and(0x0fff)
-            pass
-        elif inst == "0x2":
+            self.pc = nnn
+
+        elif inst == "0x2000":
             # calls subroutine at NNN
-            nnn = bit_and(0x0fff)
-            pass
-        elif inst == "0x3":
+            self.stack[self.sp] = self.pc
+            self.sp += 1
+            self.pc = nnn
+
+        elif inst == "0x3000":
             # Skips the next instruction if VX equals NN. (Usually the next
             # instruction is a jump to skip a code block)
-            x = bit_and(0x0f00)
-            nn = bit_and(0x00ff)
-            pass
-        elif inst == "0x4":
+            if self.v[x] == nn:
+                self.pc += 4
+            else:
+                self.pc += 2
+
+        elif inst == "0x4000":
             # Skips the next instruction if VX doesn't equal NN. (Usually the
             # next instruction is a jump to skip a code block)
-            x = bit_and(0x0f00)
-            nn = bit_and(0x00ff)
-            pass
-        elif inst == "0x5":
+            if self.v[x] != nn:
+                self.pc += 4
+            else:
+                self.pc += 2
+
+        elif inst == "0x5000":
             # Skips the next instruction if VX equals VY. (Usually the next
             # instruction is a jump to skip a code block)
-            if bit_and(0x000f) != "0":
-                raise Exception("unknown opcode")
-            x = bit_and(0x0f00)
-            y = bit_and(0x00f0)
-            pass
-        elif inst == "0x6":
+            if bit_and(instruction, 0x000f) != "0x0000":
+                raise Exception(self.ERROR_UNKNOWN_OPCODE + " " + instruction)
+            if self.v[x] == self.v[y]:
+                self.pc += 4
+            else:
+                self.pc += 2
+
+        elif inst == "0x6000":
             # Sets VX to NN.
-            x = bit_and(0x0f00)
-            nn = bit_and(0x00ff)
-            print(inst)
-            pass
-        elif inst == "0x7":
+            self.v[x] = nn
+            self.pc += 2
+
+        elif inst == "0x7000":
             # Adds NN to VX.
-            x = bit_and(0x0f00)
-            nn = bit_and(0x00ff)
-            pass
-        elif inst == "0x8":
-            last_nibble = bit_and(0x000f)
-            x = bit_and(0x0f00)
-            y = bit_and(0x00f0)
-            if last_nibble == "0":
+            self.v[x] += nn
+            self.pc += 2
+
+        elif inst == "0x8000":
+            if n == "0x0000":
                 # Sets VX to the value of VY.
-                pass
-            elif last_nibble == "1":
+                self.v[x] = self.v[y]
+
+            elif n == "0x0001":
                 # Sets VX to VX or VY. (Bitwise OR operation)
-                pass
-            elif last_nibble == "2":
+                self.v[x] = self.v[x] | self.v[y]
+
+            elif n == "0x0002":
                 # Sets VX to VX and VY. (Bitwise AND operation)
-                pass
-            elif last_nibble == "3":
+                self.v[x] = self.v[x] & self.v[y]
+
+            elif n == "0x0003":
                 # Sets VX to VX xor VY.
-                pass
-            elif last_nibble == "4":
+                self.v[x] = self.v[x] ^ self.v[y]
+
+            elif n == "0x0004":
                 # Adds VY to VX. VF is set to 1 when there's a carry, and to 0
                 # when there isn't.
-                pass
-            elif last_nibble == "5":
+                self.v[x] += self.v[y]
+                if self.v[x] > 255:
+                    self.v[0xF] = 1
+                    self.v[x] -= 256
+                else:
+                    self.v[0xF] = 0
+
+            elif n == "0x0005":
                 # VY is subtracted from VX. VF is set to 0 when there's a
                 # borrow, and 1 when there isn't.
-                pass
-            elif last_nibble == "6":
+                self.v[x] -= self.v[y]
+                if self.v[x] < 0:
+                    self.v[0xF] = 0
+                    self.v[x] += 256
+                else:
+                    self.v[0xF] = 1
+
+            elif n == "0x0006":
                 # Shifts VX right by one. VF is set to the value of the least
                 # significant bit of VX before the shift.
-                pass
-            elif last_nibble == "7":
+                self.v[0xF] = int(bin(self.v[x][-1]))
+                self.v[x] = self.v[x] >> 1
+
+            elif n == "0x0007":
                 # Sets VX to VY minus VX. VF is set to 0 when there's a borrow,
                 # and 1 when there isn't.
-                pass
-            elif last_nibble == "E":
+                self.v[x] = self.v[y] - self.v[x]
+                if self.v[x] < 0:
+                    self.v[0xF] = 0
+                    self.v[x] += 256
+                else:
+                    self.v[0xF] = 1
+
+            elif n == "0x000e":
                 # Shifts VX left by one. VF is set to the value of the most
                 # significant bit of VX before the shift.
-                pass
+                self.v[0xF] = int(bin(self.v[x][0]))
+                self.v[x] = self.v[x] << 1
+
             else:
-                raise Exception("unknown opcode")
-        elif inst == "0x9":
-            if bit_and(0x000f) != "0":
-                raise Exception("unknown opcode")
+                raise Exception(self.ERROR_UNKNOWN_OPCODE)
+            self.pc += 2
+
+        elif inst == "0x9000":
+            if bit_and(instruction, 0x000f) != "0x000":
+                raise Exception(self.ERROR_UNKNOWN_OPCODE)
             # Skips the next instruction if VX doesn't equal VY. (Usually the
             # next instruction is a jump to skip a code block)
-            x = bit_and(0x0f00)
-            y = bit_and(0x00f0)
-            pass
-        elif inst == "0xA":
+            if self.v[x] != self.v[y]:
+                self.pc += 4
+            else:
+                self.pc += 2
+
+        elif inst == "0xa000":
             # Sets I to the address NNN.
-            nnn = bit_and(0x0fff)
-            pass
-        elif inst == "0xB":
+            self.i = nnn
+            self.pc += 2
+
+        elif inst == "0xb000":
             # Jumps to the address NNN plus V0.
-            nnn = bit_and(0x0fff)
-            pass
-        elif inst == "0xC":
+            addr = self.v[0] + nnn
+            self.stack[self.sp] = self.pc
+            self.sp += 1
+            self.pc = addr
+
+        elif inst == "0xc000":
             # Sets VX to the result of a bitwise and operation on a random
             # number (Typically: 0 to 255) and NN.
-            x = bit_and(0x0f00)
-            nn = bit_and(0x00ff)
-            pass
-        elif inst == "0xD":
+            self.v[x] = nn & random.randint(0, 255)
+            self.pc += 2
+
+        elif inst == "0xd000":
             # Draws a sprite at coordinate (VX, VY) that has a width of 8
             # pixels and a height of N pixels.
-            x = bit_and(0x0f00)
-            y = bit_and(0x00f0)
-            n = bit_and(0x000f)
-        elif inst == "0xE":
-            x = bit_and(0x0f00)
-            # Skips the next instruction if the key stored in VX is pressed.
-            # (Usually the next instruction is a jump to skip a code block)
-            if bit_and(0x00ff) == "9E":
-                pass
+            self.pc += 2
+            pass
 
-            # Skips the next instruction if the key stored in VX isn't pressed.
-            # (Usually the next instruction is a jump to skip a code block)
-            elif bit_and(0x00ff) == "A1":
-                pass
+        elif inst == "0xe000":
+            if bit_and(instruction, 0x00ff) == "0x009e":
+                # Skips the next instruction if the key stored in VX is pressed.
+                # (Usually the next instruction is a jump to skip a code block)
+                if self.keys[self.v[x]] == 1:
+                    self.pc += 4
+                else:
+                    self.pc += 2
+
+            elif bit_and(instruction, 0x00ff) == "0x00a1":
+                # Skips the next instruction if the key stored in VX isn't pressed.
+                # (Usually the next instruction is a jump to skip a code block)
+                if self.keys[self.v[x]] == 0:
+                    self.pc += 4
+                else:
+                    self.pc += 2
+
             else:
-                raise Exception("unknown opcode")
-        elif inst == "0xF":
-            last_two_nibbles = bit_and(0x00ff)
-            x = bit_and(0x0f00)
-            if last_two_nibbles == "07":
-                # Sets VX to the value of the delay timer.
-                pass
+                raise Exception(self.ERROR_UNKNOWN_OPCODE)
 
-            elif last_two_nibbles == "0A":
+        elif inst == "0xf000":
+            if nn == "0x0007":
+                # Sets VX to the value of the delay timer.
+                self.v[x] = self.delay_timer
+
+            elif nn == "0x000a":
                 # A key press is awaited, and then stored in VX. (Blocking
                 # Operation. All instruction halted until next key event)
                 pass
 
-            elif last_two_nibbles == "15":
+            elif nn == "0x0015":
                 # Sets the delay timer to VX.
-                pass
+                self.delay_timer = self.v[x]
 
-            elif last_two_nibbles == "18":
+            elif nn == "0x0018":
                 # Sets the sound timer to VX.
-                pass
+                self.sound_timer = self.v[x]
 
-            elif last_two_nibbles == "1E":
+            elif nn == "0x001e":
                 # Adds VX to I.
-                pass
+                self.i += self.v[x]
 
-            elif last_two_nibbles == "29":
+            elif nn == "0x0029":
                 # Sets I to the location of the sprite for the character in VX.
                 # Characters 0-F (in hexadecimal) are represented by a 4x5
                 # font.
-                pass
+                self.i = self.v[x] * 5
 
-            elif last_two_nibbles == "33":
+            elif nn == "0x0033":
                 # Stores the binary-coded decimal representation of VX, with
                 # the most significant of three digits at the address in I, the
                 # middle digit at I plus 1, and the least significant digit at
                 # I plus 2.
-                pass
+                vx = bin(self.v[x])
+                self.memory[self.i] = vx[0]
+                try:
+                    self.memory[self.i + 1] = vx[1]
+                except:
+                    self.memory[self.i + 1] = 0
 
-            elif last_two_nibbles == "55":
+                try:
+                    self.memory[self.i + 2] = vx[2]
+                except:
+                    self.memory[self.i + 2] = 0
+
+            elif nn == "0x0055":
                 # Stores V0 to VX (including VX) in memory starting at address
                 # I.
-                pass
+                for i in range(0, 16):
+                    self.memory[self.i + i] = self.v[i]
 
-            elif last_two_nibbles == "65":
+            elif nn == "0x0065":
                 # Fills V0 to VX (including VX) with values from memory
                 # starting at address I.
-                pass
+                for i in range(0, 16):
+                    self.v[i] = self.memory[self.i + i]
+
+            self.pc += 2
+        else:
+            raise Exception(self.ERROR_UNKNOWN_OPCODE + " " + instruction)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from chip8 import Chip8
 
 
 def main():
-    ROM_NAME = "PONG"
+    ROM_NAME = "P:\Development\Python\c8emu\c8emu\PONG"
     try:
         with open(ROM_NAME, mode="rb") as file:
             rom = binascii.hexlify(file.read())
@@ -13,10 +13,13 @@ def main():
     chip8 = Chip8()
     chip8.load_rom(rom)
 
-    chip8.emulateCycle()
-    # if chip8.get_draw_flag() == True:
-    #   display.update_graphics()
-    # chip8.set_keys()
+    while(True):
+        chip8.emulateCycle()
+        if not chip8.running:
+            break
+        # if chip8.get_draw_flag() == True:
+        #   display.update_graphics()
+        # chip8.set_keys()
 
 if __name__ == '__main__':
     main()
